5b1de6404baae8f9be4747fcde079d75ca0ff719
Abner299 <89983277+Abner299@users.noreply.github.com>
2025-02-09 22:47:41 -0400

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-// Alterna entre as páginas
 function showPage(pageId) {
+    // Remove a classe "active" de todos os itens do menu
+    document.querySelectorAll('.sidebar ul li').forEach(item => {
+        item.classList.remove('active');
+    });
+
+    // Adiciona a classe "active" no item do menu correspondente
+    document.querySelector(`.sidebar ul li[onclick="showPage('${pageId}')"]`).classList.add('active');
+
+    // Alterna entre as páginas
     document.querySelectorAll('.page').forEach(page => {
         page.classList.remove('active');
     });
     document.getElementById(pageId).classList.add('active');
-}
-
-// Abrir e fechar formulário na recepção
-function abrirFormulario() {
-    document.getElementById("formulario-recepcao").style.display = "flex";
-}
-
-function fecharFormulario() {
-    document.getElementById("formulario-recepcao").style.display = "none";
 }
